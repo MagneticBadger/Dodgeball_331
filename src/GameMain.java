a92cef55606fd906240cc5e4eb008462f0f43b0c
toddthegodd <hts8504@uncw.edu>
2016-04-24 17:07:51 -0400

    Difficulty Functions. 4 levels: Easy, Medium, Hard, Insane

--- src/GameMain.java
+++ src/GameMain.java
@@ -13,7 +13,7 @@ import static java.lang.System.nanoTime;
 public class GameMain extends JFrame {
     Thread thread;
     private final long framePeriod = 1000000000 / 30;
-    private int  difficulty, fps = 30;
+    private int  difficulty = 0, fps = 30;
     private double startTime, currentTime;
     private long gameEndTime, runTime;                // done in seconds for checking, minutes:seconds for leaderboard menu
     private boolean isPaused, isEnded;
@@ -34,11 +34,24 @@ public class GameMain extends JFrame {
         setSize(750, 750);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         player = new Player(getWidth()/2, getHeight()/2);
-        generateOpponents(4);
+
         // init power ups
 
         // set difficulty - handled in menu
         // if not, jdialog
+        if(difficulty == 0) {
+            generateOpponents(4);
+        }
+        else if(difficulty == 1){
+            generateOpponents(4);
+        }
+        else if(difficulty == 2){
+            generateOpponents(6);
+        }
+        else if(difficulty == 3){
+            generateOpponents(6);
+        }
+
         thread = new Thread();
         thread.start();
         setVisible(true);
@@ -72,30 +85,56 @@ public class GameMain extends JFrame {
      * @param numberToAdd
      */
     private void generateOpponents(int numberToAdd) {
+        int minVelocity;
+        int maxVelocity;
+        if(difficulty == 0 | difficulty == 2){
+            minVelocity = 8;
+            maxVelocity = 20;
+        }
+        else{
+            minVelocity = 12;
+            maxVelocity = 30;
+        }
 
         if (numberToAdd == 4) {
             // Top left block
-            balls.add(new Ball(20 + OPP_RADIUS, 20 + OPP_RADIUS, OPP_RADIUS, 20, 30));
+            balls.add(new Ball(20 + OPP_RADIUS, 20 + OPP_RADIUS, OPP_RADIUS,
+                    minVelocity + Math.random() * (maxVelocity - minVelocity), minVelocity + Math.random() * (maxVelocity - minVelocity)));
             // top right block
-            balls.add(new Ball(getWidth() - OPP_RADIUS, 20 + OPP_RADIUS, OPP_RADIUS, -22, 27));
+            balls.add(new Ball(getWidth() - OPP_RADIUS, 20 + OPP_RADIUS, OPP_RADIUS,
+                    -(minVelocity + Math.random() * (maxVelocity - minVelocity)), minVelocity + Math.random() * (maxVelocity - minVelocity)));
             // bottom left
-            balls.add(new Ball(20, getHeight() - OPP_RADIUS, OPP_RADIUS + 20, 12, -25));
+            balls.add(new Ball(20, getHeight() - OPP_RADIUS, OPP_RADIUS + 20,
+                    minVelocity + Math.random() * (maxVelocity - minVelocity), -(minVelocity + Math.random() * (maxVelocity - minVelocity))));
             // bottom right
-            balls.add(new Ball(getWidth() - OPP_RADIUS - 20, getHeight() - OPP_RADIUS - 20, OPP_RADIUS, -15, -19));
+            balls.add(new Ball(getWidth() - OPP_RADIUS - 20, getHeight() - OPP_RADIUS - 20, OPP_RADIUS,
+                    -(minVelocity + Math.random() * (maxVelocity - minVelocity)), -(minVelocity + Math.random() * (maxVelocity - minVelocity))));
 
         } else if(numberToAdd == 6) {
+
             // Top left block
-            balls.add(new Ball(20 + OPP_RADIUS, 20 + OPP_RADIUS, OPP_RADIUS, 20, 30));
+            balls.add(new Ball(20 + OPP_RADIUS, 20 + OPP_RADIUS, OPP_RADIUS,
+                    minVelocity + Math.random() * (maxVelocity - minVelocity), minVelocity + Math.random() * (maxVelocity - minVelocity)));
+
             // top right block
-            balls.add(new Ball(getWidth() - OPP_RADIUS, 20 + OPP_RADIUS, OPP_RADIUS, -22, 27));
+            balls.add(new Ball(getWidth() - OPP_RADIUS, 20 + OPP_RADIUS, OPP_RADIUS,
+                    -(minVelocity + Math.random() * (maxVelocity - minVelocity)), minVelocity + Math.random() * (maxVelocity - minVelocity)));
+
             // bottom left
-            balls.add(new Ball(20, getHeight() - OPP_RADIUS, OPP_RADIUS + 20, 12, -25));
+            balls.add(new Ball(20, getHeight() - OPP_RADIUS, OPP_RADIUS + 20,
+                    minVelocity + Math.random() * (maxVelocity - minVelocity), -(minVelocity + Math.random() * (maxVelocity - minVelocity))));
+
             // bottom right
-            balls.add(new Ball(getWidth() - OPP_RADIUS - 20, getHeight() - OPP_RADIUS - 20, OPP_RADIUS, -15, -19));
+            balls.add(new Ball(getWidth() - OPP_RADIUS - 20, getHeight() - OPP_RADIUS - 20, OPP_RADIUS,
+                    -(minVelocity + Math.random() * (maxVelocity - minVelocity)), -(minVelocity + Math.random() * (maxVelocity - minVelocity))));
+
             // Top Middle block
-            balls.add(new Ball(getWidth() / 2, OPP_RADIUS, OPP_RADIUS, 12, 19));
+            balls.add(new Ball(getWidth() / 2, OPP_RADIUS, OPP_RADIUS,
+                    minVelocity + Math.random() * (maxVelocity - minVelocity), minVelocity + Math.random() * (maxVelocity - minVelocity)));
+
             // bottom middle block
-            balls.add(new Ball(getWidth() / 2, getHeight() - OPP_RADIUS, OPP_RADIUS, -8, 11.5));
+            balls.add(new Ball(getWidth() / 2, getHeight() - OPP_RADIUS, OPP_RADIUS,
+                    -(minVelocity + Math.random() * (maxVelocity - minVelocity)), (minVelocity + Math.random() * (maxVelocity - minVelocity))));
         }
     }
 
