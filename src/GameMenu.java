6725ecf01a3cf39f7eaab58f661581c6810b4b73
toddthegodd <hts8504@uncw.edu>
2016-04-25 17:26:47 -0400

    Beginnings of Menu class. Needs much work

--- src/GameMenu.java
+++ src/GameMenu.java
@@ -1,6 +1,21 @@
 import javax.swing.*;
+import java.awt.*;
 
 
 public class GameMenu extends JPanel {
+
+    public void GameMenu(){
+        setLayout(new BorderLayout());
+        ImageIcon icon = new ImageIcon("StartButton.png");
+        JButton start = new JButton(icon);
+        JButton leaderboard = new JButton(icon);
+        JButton exit = new JButton(icon);
+        this.add(start);
+        this.add(leaderboard);
+        this.add(exit);
+    }
+
+
+
 
 }
